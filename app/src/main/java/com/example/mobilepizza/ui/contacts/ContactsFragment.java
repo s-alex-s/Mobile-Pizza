a8da5d64d3dd698db750529cc5a8bda5c17b9e55
Alex <s-alex-smir@yandex.ru>
2022-05-13 16:07:29 +0600

    contacts fragment, layout fix, contact to support button and MobilePizza email

--- app/src/main/java/com/example/mobilepizza/ui/contacts/ContactsFragment.java
+++ app/src/main/java/com/example/mobilepizza/ui/contacts/ContactsFragment.java
@@ -1,5 +1,8 @@
 package com.example.mobilepizza.ui.contacts;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +10,40 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 import com.example.mobilepizza.R;
 
 public class ContactsFragment extends Fragment {
+    WebView webView;
+    WebSettings webSettings;
+    Button button;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+        webView = view.findViewById(R.id.map_view);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://yandex.ru/maps/?um=constructor%3A70396694c6ae8db683b4c48f7160cd7fe1f9f3d63dd8b60c90892980dfc25175&source=constructorLink");
+
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        button = view.findViewById(R.id.contact_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_SENDTO)
+                        .setData(Uri.parse("mailto:mobile.pizza2022@gmail.com"))
+                );
+            }
+        });
+
+        return view;
     }
 }
