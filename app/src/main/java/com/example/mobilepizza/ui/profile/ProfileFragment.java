1f008cc5ba66e690b4e53fd87dd62ebae376a5fc
Alex <s-alex-smir@yandex.ru>
2022-05-29 14:50:16 +0600

    profile fragment is completed, fix bugs, added possibility to add phone number and added display email address for account with google sign in provider.

--- app/src/main/java/com/example/mobilepizza/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/mobilepizza/ui/profile/ProfileFragment.java
@@ -1,18 +1,25 @@
 package com.example.mobilepizza.ui.profile;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
-import com.squareup.picasso.Picasso;
 import com.example.mobilepizza.AuthActivity;
 import com.example.mobilepizza.R;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -20,19 +27,32 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Objects;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.hbb20.CountryCodePicker;
+import com.squareup.picasso.Picasso;
 
 public class ProfileFragment extends Fragment {
 
+    View view;
+
     FirebaseAuth mAuth;
     FirebaseUser currentUser;
+    FirebaseDatabase database;
+    DatabaseReference db_ref;
+
+    ProgressBar progressBar;
 
     // Google profile elements
     ConstraintLayout google_profile;
     ImageView google_profile_photo;
-    TextView google_name, google_phone;
-    Button google_logout_button;
+    TextView google_name, google_phone, google_email, phone_title;
+    Button google_logout_button, add_phone_number;
+    CardView phone_card;
 
     // Phone profile elements
     ConstraintLayout phone_profile;
@@ -43,31 +63,111 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
 
-        if (currentUser.getProviderData().get(1).getProviderId().equals("google.com")) {
-            google_profile = view.findViewById(R.id.google_profile);
-            google_profile.setVisibility(View.VISIBLE);
+        database = FirebaseDatabase.getInstance();
+        db_ref = database.getReference();
 
-            google_profile_photo = view.findViewById(R.id.google_profile_photo);
+        progressBar = view.findViewById(R.id.progressBar2);
 
-            Picasso.get().load(currentUser.getPhotoUrl()).into(google_profile_photo);
+        // Google profile elements
+        google_profile = view.findViewById(R.id.google_profile);
+        add_phone_number = view.findViewById(R.id.add_phone_number_button);
+        phone_card = view.findViewById(R.id.card_google_phone);
+        phone_title = view.findViewById(R.id.google_phone_title);
+        google_profile_photo = view.findViewById(R.id.google_profile_photo);
+        google_name = view.findViewById(R.id.google_name);
+        google_phone = view.findViewById(R.id.google_phone);
+        google_logout_button = view.findViewById(R.id.google_logout_button);
+        google_email = view.findViewById(R.id.google_email);
 
-            google_name = view.findViewById(R.id.google_name);
-            google_name.setText(currentUser.getDisplayName());
+        // Phone profile elements
+        phone_logout_button = view.findViewById(R.id.phone_logout_button);
+        phone_phone_number = view.findViewById(R.id.phone_phone_number);
+        phone_profile = view.findViewById(R.id.phone_profile);
 
-            google_phone = view.findViewById(R.id.google_phone);
-            if (!Objects.equals(currentUser.getPhoneNumber(), "")) {
-                google_phone.setText(currentUser.getPhoneNumber());
-            } else {
-                view.findViewById(R.id.card_google_phone).setVisibility(View.GONE);
-                view.findViewById(R.id.google_phone_title).setVisibility(View.GONE);
+        getUserProfile(view);
+
+        return view;
+    }
+
+    public void showDialog(Activity activity) {
+        final Dialog dialog = new Dialog(activity);
+        dialog.setContentView(R.layout.get_phone_number);
+
+        EditText editText = dialog.findViewById(R.id.editTextPhone_dialog);
+        CountryCodePicker ccp = dialog.findViewById(R.id.code_picker_dialog);
+        Button button = dialog.findViewById(R.id.add_phone_number_dialog);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (editText.getText().toString().length() == 10) {
+                    String phone_input = ccp.getSelectedCountryCodeWithPlus() + editText.getText().toString();
+
+                    db_ref.child("users").child(currentUser.getUid()).child("phone_number").setValue(phone_input);
+
+                    add_phone_number.setText(getString(R.string.edit_phone_number));
+                    Toast.makeText(view.getContext(), getString(R.string.add_phone_success), Toast.LENGTH_SHORT).show();
+
+                    dialog.dismiss();
+
+                    google_phone.setText(phone_input);
+
+                    phone_card.setVisibility(View.VISIBLE);
+                    phone_title.setVisibility(View.VISIBLE);
+                } else {
+                    editText.setError(getString(R.string.number_error));
+                }
             }
+        });
 
-            google_logout_button = view.findViewById(R.id.google_logout_button);
+        dialog.show();
+    }
+
+    public void getUserProfile(View view) {
+        if (currentUser.getProviderData().get(1).getProviderId().equals("google.com")) {
+
+            UserInfo profile = currentUser.getProviderData().get(1);
+
+            add_phone_number.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showDialog(getActivity());
+                }
+            });
+
+            Picasso.get().load(profile.getPhotoUrl()).into(google_profile_photo);
+
+            google_name.setText(profile.getDisplayName());
+            db_ref.child("users").child(currentUser.getUid()).child("phone_number").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (!String.valueOf(snapshot.getValue()).equals("null")) {
+                        add_phone_number.setText(getString(R.string.edit_phone_number));
+                        google_phone.setText(snapshot.getValue().toString());
+                        google_phone.setVisibility(View.VISIBLE);
+                    } else {
+                        view.findViewById(R.id.card_google_phone).setVisibility(View.GONE);
+                        view.findViewById(R.id.google_phone_title).setVisibility(View.GONE);
+                    }
+                    progressBar.setVisibility(View.GONE);
+                    google_profile.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.e("firebase", error.getMessage());
+                    Toast.makeText(view.getContext(), getString(R.string.error), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            google_email.setText(profile.getEmail());
+
             google_logout_button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -83,13 +183,8 @@ public class ProfileFragment extends Fragment {
                 }
             });
         } else if (currentUser.getProviderData().get(1).getProviderId().equals("phone")) {
-            phone_profile = view.findViewById(R.id.phone_profile);
             phone_profile.setVisibility(View.VISIBLE);
-
-            phone_phone_number = view.findViewById(R.id.phone_phone_number);
             phone_phone_number.setText(currentUser.getPhoneNumber());
-
-            phone_logout_button = view.findViewById(R.id.phone_logout_button);
             phone_logout_button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -99,8 +194,8 @@ public class ProfileFragment extends Fragment {
                             .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
                 }
             });
-        }
 
-        return view;
+            progressBar.setVisibility(View.GONE);
+        }
     }
 }
