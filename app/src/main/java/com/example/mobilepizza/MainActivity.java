4942f06d87b25858bafbae32240c0414a858329e
Alex <s-alex-smir@yandex.ru>
2022-06-12 14:57:54 +0600

    check internet connection

--- app/src/main/java/com/example/mobilepizza/MainActivity.java
+++ app/src/main/java/com/example/mobilepizza/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.mobilepizza;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.net.NetworkRequest;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
@@ -38,7 +48,70 @@ public class MainActivity extends AppCompatActivity {
         ).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host);
         NavigationUI.setupWithNavController(navView, navController);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            NetworkRequest networkRequest = new NetworkRequest.Builder()
+                    .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                    .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                    .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+                    .build();
+
+            ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback() {
+                @Override
+                public void onAvailable(@NonNull Network network) {
+                    super.onAvailable(network);
+                }
+
+                @Override
+                public void onLost(@NonNull Network network) {
+                    super.onLost(network);
+
+                    lostConnectionDialog();
+                }
+
+                @Override
+                public void onCapabilitiesChanged(@NonNull Network network, @NonNull NetworkCapabilities networkCapabilities) {
+                    super.onCapabilitiesChanged(network, networkCapabilities);
+                    final boolean unmetered = networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED);
+                }
+            };
+
+            ConnectivityManager connectivityManager =
+                    (ConnectivityManager) getSystemService(ConnectivityManager.class);
+            connectivityManager.requestNetwork(networkRequest, networkCallback);
+        }
     }
+
+    public boolean isNotOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo == null || !netInfo.isConnectedOrConnecting();
+    }
+
+    public void lostConnectionDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.connection_lost));
+        builder.setMessage(getString(R.string.repeat_connection_later));
+        builder.setCancelable(false);
+
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (i == DialogInterface.BUTTON_POSITIVE) {
+                    if (isNotOnline()) {
+                        lostConnectionDialog();
+                    }
+                    dialogInterface.dismiss();
+                }
+            }
+        };
+
+        builder.setPositiveButton(getString(R.string.repeat_connection), listener);
+
+        builder.create().show();
+    }
+
 
     @Override
     protected void onStart() {
@@ -47,6 +120,12 @@ public class MainActivity extends AppCompatActivity {
         if (mAuth.getCurrentUser() == null) {
             startActivity(new Intent(this, AuthActivity.class));
             finish();
+        }
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (isNotOnline()) {
+                lostConnectionDialog();
+            }
         }
     }
 }
